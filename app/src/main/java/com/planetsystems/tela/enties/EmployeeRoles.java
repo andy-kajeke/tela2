81b93f87d45a39ffaf4774a9960c04686f1a7826
simonojok19 <simonojok19@gmail.com>
2020-03-16 14:35:56 +0300

    added employee role table complete

--- app/src/main/java/com/planetsystems/tela/enties/EmployeeRoles.java
+++ app/src/main/java/com/planetsystems/tela/enties/EmployeeRoles.java
@@ -1,5 +1,28 @@
 package com.planetsystems.tela.enties;
 
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "EmployeeRoles")
 public class EmployeeRoles {
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private String id;
+
+    @ColumnInfo(name = "dateCreated")
+    private String dateCreated;
+
+    @ColumnInfo(name = "dateUpdated")
+    private String dateUpdated;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "employeeRole")
+    private String employeeRole;
+
+    @ColumnInfo(name = "status")
+    private String status;
 }
