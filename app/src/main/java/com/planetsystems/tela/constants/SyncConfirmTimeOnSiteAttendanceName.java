84da8cabbdc0198353530a548e97c35095d818b2
simonojok19 <simonojok19@gmail.com>
2020-03-16 20:14:35 +0300

    added refactory

--- app/src/main/java/com/planetsystems/tela/constants/SyncConfirmTimeOnSiteAttendanceName.java
+++ app/src/main/java/com/planetsystems/tela/constants/SyncConfirmTimeOnSiteAttendanceName.java
@@ -4,7 +4,7 @@ public class SyncConfirmTimeOnSiteAttendanceName {
     public static final String TABLE_NAME = "sync_confirm_time_on_site_attendance";
     public static final String ID = "id";
     public static final String DATE_CREATED = "dateCreated";
-    public static final String DATE_UPDATE = "dateUpdated";
+    public static final String DATE_UPDATED = "dateUpdated";
     public static final String STATUS = "status";
     public static final String EMPLOYEE_ID = "employeeId";
     public static final String EMPLOYEE_NO = "employeeNo";
--- app/src/main/java/com/planetsystems/tela/enties/SyncConfirmTimeOnSiteAttendance.java
+++ app/src/main/java/com/planetsystems/tela/enties/SyncConfirmTimeOnSiteAttendance.java
@@ -1,4 +1,52 @@
 package com.planetsystems.tela.enties;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.planetsystems.tela.constants.SyncConfirmTimeOnSiteAttendanceName;
+
+@Entity(tableName = SyncConfirmTimeOnSiteAttendanceName.TABLE_NAME)
 public class SyncConfirmTimeOnSiteAttendance {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.ID)
+    private String id;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.DATE_CREATED)
+    private String dateCreated;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.DATE_UPDATED)
+    private String dateUpdated;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.STATUS)
+    private String status;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.EMPLOYEE_ID)
+    private String employeeId;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.EMPLOYEE_NO)
+    private String employeeNo;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.SUPERVISION_DATE)
+    private String supervisionDate;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.SUPERVISION_DAY)
+    private String supervisionDay;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.SUPERVISION_STATUS)
+    private String supervisionStatus;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.SUPERVISION_COMMENT)
+    private String supervisionComment;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.SUPERVISOR_ID)
+    private String supervisionId;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.ROW_VER)
+    private String rowVer;
+
+    @ColumnInfo(name = SyncConfirmTimeOnSiteAttendanceName.ROW_ID)
+    private String rowId;
 }
