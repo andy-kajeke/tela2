ade7b5399e381b10a537a027309ca024d148cb15
simonojok19 <simonojok19@gmail.com>
2020-03-16 21:05:13 +0300

    added dao

--- app/src/main/java/com/planetsystems/tela/dao/SyncEmployeeMaterialRequestDao.java
+++ app/src/main/java/com/planetsystems/tela/dao/SyncEmployeeMaterialRequestDao.java
@@ -1,4 +1,21 @@
 package com.planetsystems.tela.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.planetsystems.tela.constants.SyncEmployeeMaterialRequestName;
+import com.planetsystems.tela.enties.SyncEmployeeMaterialRequest;
+
+import java.util.List;
+
+@Dao
 public interface SyncEmployeeMaterialRequestDao {
+    @Insert
+    void insertSyncEmployeeMaterialRequest(SyncEmployeeMaterialRequest materialRequest);
+
+    @Query("SELECT * FROM " + SyncEmployeeMaterialRequestName.TABLE_NAME)
+    LiveData<List<SyncEmployeeMaterialRequest>> getAllSyncEmployeeMaterialRequest();
+
 }
