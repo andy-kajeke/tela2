ce9040c20d9d948b77c741c1a53f49506ee20f5e
simonojok19 <simonojok19@gmail.com>
2020-03-16 14:43:57 +0300

    added spacing

--- app/src/main/java/com/planetsystems/tela/enties/SyncTeacher.java
+++ app/src/main/java/com/planetsystems/tela/enties/SyncTeacher.java
@@ -11,37 +11,59 @@ public class SyncTeacher {
     @ColumnInfo(name = "id")
     @NonNull
     private String id;
+
     @ColumnInfo(name = "MPSComputerNumber")
     private String MPSComputerNumber;
+
+
     @ColumnInfo(name = "dob")
     @NonNull
     private String dob;
+
+
     @ColumnInfo(name = "emailAddress")
     private String emailAddress;
+
+
     @ColumnInfo(name = "fingerPrint")
     @NonNull
     private Byte[] fingerPrint;
+
+
     @ColumnInfo(name = "firstName")
     @NonNull
     private String firstName;
+
+
     @ColumnInfo(name = "lastName")
     @NonNull
     private String lastName;
+
+
     @ColumnInfo(name = "gender")
     @NonNull
     private String gender;
+
+
     @ColumnInfo(name = "initials")
     @NonNull
     private String initials;
+
     @ColumnInfo(name = "licensed")
     @NonNull
     private int licensed;
+
+
     @ColumnInfo(name = "nationalId")
     @NonNull
     private String nationalId;
+
+
     @ColumnInfo(name = "phoneNumber")
     @NonNull
     private String phoneNumber;
+
+
     @ColumnInfo(name = "schoolId")
     @NonNull
     private String schoolId;
