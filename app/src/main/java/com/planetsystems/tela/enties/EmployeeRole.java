d4baf1a8532690516e6cf79c977649925cc4cb8f
simonojok19 <simonojok19@gmail.com>
2020-03-16 14:39:13 +0300

    added toString constructor and getters4

--- app/src/main/java/com/planetsystems/tela/enties/EmployeeRole.java
+++ app/src/main/java/com/planetsystems/tela/enties/EmployeeRole.java
@@ -1,6 +1,7 @@
 package com.planetsystems.tela.enties;
 
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -25,4 +26,67 @@ public class EmployeeRole {
 
     @ColumnInfo(name = "status")
     private String status;
+
+    public EmployeeRole(String id, String dateCreated, String dateUpdated, String description, String employeeRole, String status) {
+        this.id = id;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+        this.description = description;
+        this.employeeRole = employeeRole;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(String dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmployeeRole() {
+        return employeeRole;
+    }
+
+    public void setEmployeeRole(String employeeRole) {
+        this.employeeRole = employeeRole;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Teacher Role " + this.id;
+    }
 }
