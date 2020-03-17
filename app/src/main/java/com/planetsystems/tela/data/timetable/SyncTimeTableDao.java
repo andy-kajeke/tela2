4045cd52bd105158d343fd9054dcaa63a59aad4b
simonojok19 <simonojok19@gmail.com>
2020-03-17 15:57:28 +0300

    added dao for sync table

---
+++ app/src/main/java/com/planetsystems/tela/data/timetable/SyncTimeTableDao.java
@@ -0,0 +1,17 @@
+package com.planetsystems.tela.data.timetable;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SyncTimeTableDao {
+    @Insert
+    void insertSyncTimeTable(SyncTimeTable syncTimeTable);
+
+    @Query("SELECT * FROM " + SyncTimeTableConstant.TABLE_NAME)
+    LiveData<List<SyncTimeTable>> getSyncTimeTables();
+}
