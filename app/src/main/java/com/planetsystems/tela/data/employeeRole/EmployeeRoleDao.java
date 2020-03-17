780e6609cc6808e88bef149a9e51efdcc1d25e28
simonojok19 <simonojok19@gmail.com>
2020-03-17 09:38:52 +0300

    employee role

--- app/src/main/java/com/planetsystems/tela/data/employeeRole/EmployeeRoleDao.java
+++ app/src/main/java/com/planetsystems/tela/data/employeeRole/EmployeeRoleDao.java
@@ -1,4 +1,31 @@
 package com.planetsystems.tela.data.employeeRole;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.planetsystems.tela.constants.EmployeeRoleName;
+import com.planetsystems.tela.enties.EmployeeRole;
+
+import java.util.List;
+
+@Dao
 public interface EmployeeRoleDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertEmployeeRole(com.planetsystems.tela.enties.EmployeeRole employeeRole);
+
+    @Query("SELECT * from " + EmployeeRoleName.TABLE_NAME)
+    LiveData<List<com.planetsystems.tela.enties.EmployeeRole>> queryAllEmployeeRoles();
+
+    @Update(entity = com.planetsystems.tela.enties.EmployeeRole.class)
+    void updateStudent(com.planetsystems.tela.enties.EmployeeRole employeeRole);
+
+    @Delete(entity = com.planetsystems.tela.enties.EmployeeRole.class)
+    void deleteStudent(EmployeeRole employeeRole);
+
 }
