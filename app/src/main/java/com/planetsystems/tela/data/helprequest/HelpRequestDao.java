6bdabf6d2b38c213c954f616402656b3e61a8359
simonojok19 <simonojok19@gmail.com>
2020-03-17 13:57:58 +0300

    added the dao

--- app/src/main/java/com/planetsystems/tela/data/helprequest/HelpRequestDao.java
+++ app/src/main/java/com/planetsystems/tela/data/helprequest/HelpRequestDao.java
@@ -1,4 +1,18 @@
 package com.planetsystems.tela.data.helprequest;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface HelpRequestDao {
+
+    @Insert
+    void insertHelpRequest(HelpRequest helpRequest4);
+
+    @Query("SELECT * FROM " + HelpRequestConstant.TABLE_NAME)
+    LiveData<List<HelpRequest>> getHelpRequests();
 }
