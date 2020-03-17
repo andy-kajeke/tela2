4bcf225a85324904d2b0dcfbe7deff9c7d4a5045
simonojok19 <simonojok19@gmail.com>
2020-03-17 15:19:49 +0300

    implemented dao

--- app/src/main/java/com/planetsystems/tela/data/timetble/SyncTimeTableDao.java
+++ app/src/main/java/com/planetsystems/tela/data/timetble/SyncTimeTableDao.java
@@ -1,7 +1,17 @@
 package com.planetsystems.tela.data.timetble;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
-public abstract class SyncTimeTableDao {
+public interface SyncTimeTableDao {
+    @Insert
+    void inertSyncTime(SyncTimeTable syncTimeTable);
+
+    @Query("SELECT * FROM " + SyncTimeTableConstant.TABLE_NAME)
+    LiveData<List<SyncTimeTable>> getSyncTimeBables();
 }
