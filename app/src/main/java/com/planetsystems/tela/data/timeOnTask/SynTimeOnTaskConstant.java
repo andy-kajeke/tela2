2fda9c7066c0a62fbc4465d342e1cc1ad81741f0
simonojok19 <simonojok19@gmail.com>
2020-03-17 15:54:51 +0300

    done entities

--- app/src/main/java/com/planetsystems/tela/data/timeOnTask/SynTimeOnTaskConstant.java
+++ app/src/main/java/com/planetsystems/tela/data/timeOnTask/SynTimeOnTaskConstant.java
@@ -1,23 +1,23 @@
 package com.planetsystems.tela.data.timeOnTask;
 
-public class SynTimeOnTaskConstant {
-    public static final String TABLE_NAME = "sync_time_on_tasks";
-    public static final String ID = "id";
-    public static final String DATE_CREATED = "dateCreated";
-    public static final String DATE_UPDATED = "dateUpdated";
-    public static final String STATUS = "status";
-    public static final String ACTION_STATUS = "actionStatus";
-    public static final String COMMENT = "comment";
-    public static final String EMPLOYEE_ID = "employeeId";
-    public static final String EMPLOYEE_NUMBER = "employeeNumber";
-    public static final String TASK_ID = "taskId";
-    public static final String TRANSACTION_TIME = "transactionTime";
-    public static final String TRANSACTION_DATE = "transactionDate";
-    public static final String ROW_VER = "rowVer";
-    public static final String ROW_ID = "rowID";
-    public static final String EMPLOYEE_FIRST_NAME = "employeeName";
-    public static final String EMPLOYEE_LAST_NAME = "employeeLastName";
-    public static final String END_TIME = "endTime";
-    public static final String START_TIME = "startTime";
-    public static final String TASK_NAME = "taskName";
+class SynTimeOnTaskConstant {
+    static final String TABLE_NAME = "sync_time_on_tasks";
+    static final String ID = "id";
+    static final String DATE_CREATED = "dateCreated";
+    static final String DATE_UPDATED = "dateUpdated";
+    static final String STATUS = "status";
+    static final String ACTION_STATUS = "actionStatus";
+    static final String COMMENT = "comment";
+    static final String EMPLOYEE_ID = "employeeId";
+    static final String EMPLOYEE_NUMBER = "employeeNumber";
+    static final String TASK_ID = "taskId";
+    static final String TRANSACTION_TIME = "transactionTime";
+    static final String TRANSACTION_DATE = "transactionDate";
+    static final String ROW_VER = "rowVer";
+    static final String ROW_ID = "rowID";
+    static final String EMPLOYEE_FIRST_NAME = "employeeName";
+    static final String EMPLOYEE_LAST_NAME = "employeeLastName";
+    static final String END_TIME = "endTime";
+    static final String START_TIME = "startTime";
+    static final String TASK_NAME = "taskName";
 }
--- app/src/main/java/com/planetsystems/tela/data/timetable/SyncTimeTable.java
+++ app/src/main/java/com/planetsystems/tela/data/timetable/SyncTimeTable.java
@@ -62,4 +62,170 @@ public class SyncTimeTable {
 
     @ColumnInfo(name = SyncTimeTableConstant.ROW_VER)
     private String rowVer;
+
+    public SyncTimeTable(@NonNull String id, String dateCreated, String dateUpdated, String status, String day, String employeeName, String employeeId, String employeeNo, String endTime, String schoolId, String schoolName, String startTime, String subject, String subjectId, String taskId, String taskName, String rowId, String rowVer) {
+        this.id = id;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+        this.status = status;
+        this.day = day;
+        this.employeeName = employeeName;
+        this.employeeId = employeeId;
+        this.employeeNo = employeeNo;
+        this.endTime = endTime;
+        this.schoolId = schoolId;
+        this.schoolName = schoolName;
+        this.startTime = startTime;
+        this.subject = subject;
+        this.subjectId = subjectId;
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.rowId = rowId;
+        this.rowVer = rowVer;
+    }
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(String dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeNo() {
+        return employeeNo;
+    }
+
+    public void setEmployeeNo(String employeeNo) {
+        this.employeeNo = employeeNo;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getRowId() {
+        return rowId;
+    }
+
+    public void setRowId(String rowId) {
+        this.rowId = rowId;
+    }
+
+    public String getRowVer() {
+        return rowVer;
+    }
+
+    public void setRowVer(String rowVer) {
+        this.rowVer = rowVer;
+    }
 }
