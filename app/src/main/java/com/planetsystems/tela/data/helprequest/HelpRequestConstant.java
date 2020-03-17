9b2830783f0b87ec76875b5a6903bdf54d7d9ff8
simonojok19 <simonojok19@gmail.com>
2020-03-17 13:14:44 +0300

    added static fields

--- app/src/main/java/com/planetsystems/tela/data/helprequest/HelpRequestConstant.java
+++ app/src/main/java/com/planetsystems/tela/data/helprequest/HelpRequestConstant.java
@@ -1,4 +1,20 @@
 package com.planetsystems.tela.data.helprequest;
 
 public class HelpRequestConstant {
+    public static final String ID = "id";
+    public static final String DATE_CREATED = "dateCreated";
+    public static final String DATE_UPDATED = "dateUpdated";
+    public static final String STATUS = "status";
+    public static final String APPROVED_STATUS = "approvedStatus";
+    public static final String CONFIRMATION = "confirmation";
+    public static final String COMMENT = "comment";
+    public static final String DEPLOYMENT_SITE = "deploymentSite";
+    public static final String DEPLOYMENT_SITE_ID = "deploymentSiteId";
+    public static final String HELP_CATEGORY = "helpCategory";
+    public static final String PRIORITY = "priority";
+    public static final String STAFF_CODE = "staffCode";
+    public static final String ROW_VER = "rowVer";
+    public static final String ROW_ID = "rowId";
+    public static final String REQUESTED_DATE = "requestedDate";
+    public static final String APPROVAL_DATE = "approvalDate";
 }
