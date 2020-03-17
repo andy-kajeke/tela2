b1eef95dd9193906df1f3e941016e980fd3e707e
andy-kajeke <akajeke@gmail.com>
2020-03-17 15:58:50 +0300

    enroll template

--- app/src/main/java/com/planetsystems/tela/activities/ClockIn_ClockOut.java
+++ app/src/main/java/com/planetsystems/tela/activities/ClockIn_ClockOut.java
@@ -2,15 +2,55 @@ package com.planetsystems.tela.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.planetsystems.tela.R;
 
+import java.text.SimpleDateFormat;
+
 public class ClockIn_ClockOut extends AppCompatActivity {
 
+    TextView dateDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clockin__clock_out);
+
+        dateDisplay = findViewById(R.id.calendarView4);
+
+        //Display the date to ui
+        long date = System.currentTimeMillis();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd /MM/ yyy");
+        String dateString = dateFormat.format(date);
+        dateDisplay.setText(dateString);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.enroll:
+                Intent home = new Intent(ClockIn_ClockOut.this, EnrollmentActivity.class);
+                startActivity(home);
+                return true;
+            case R.id.settings:
+                //showHelp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
