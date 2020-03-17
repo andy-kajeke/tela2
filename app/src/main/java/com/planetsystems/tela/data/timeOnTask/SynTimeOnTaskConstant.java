0845209dcdef47da4b0e75f6e2726bc274c0ec67
simonojok19 <simonojok19@gmail.com>
2020-03-17 12:34:10 +0300

    added constants

--- app/src/main/java/com/planetsystems/tela/data/timeOnTask/SynTimeOnTaskConstant.java
+++ app/src/main/java/com/planetsystems/tela/data/timeOnTask/SynTimeOnTaskConstant.java
@@ -1,4 +1,22 @@
 package com.planetsystems.tela.data.timeOnTask;
 
 public class SynTimeOnTaskConstant {
+    public static final String ID = "id";
+    public static final String DATE_CREATED = "dateCreated";
+    public static final String DATE_UPDATED = "dateUpdated";
+    public static final String STATUS = "status";
+    public static final String ACTION_STATUS = "actionStatus";
+    public static final String COMMENT = "comment";
+    public static final String EMPLOYEE_ID = "employeeId";
+    public static final String EMPLOYEE_NUMBER = "employeeNumber";
+    public static final String TASK_ID = "taskId";
+    public static final String TRANSACTION_TIME = "transactionTime";
+    public static final String TRANSACTION_DATE = "transactionDate";
+    public static final String ROW_VER = "rowVer";
+    public static final String ROW_ID = "rowID";
+    public static final String EMPLOYEE_FIRST_NAME = "employeeName";
+    public static final String EMPLOYEE_LAST_NAME = "employeeLastName";
+    public static final String END_TIME = "endTime";
+    public static final String START_TIME = "startTime";
+    public static final String TASK_TIME = "taskTime";
 }
