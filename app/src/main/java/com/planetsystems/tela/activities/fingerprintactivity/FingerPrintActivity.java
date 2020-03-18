51392f2b222a7ede39aee187afc47c69a976b5ae
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-18 16:43:05 +0300

    adding codes for sprema

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -2,11 +2,15 @@ package com.planetsystems.tela.activities.fingerprintactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.planetsystems.tela.R;
+import com.suprema.IBioMiniDevice;
 
 public class FingerPrintActivity extends AppCompatActivity {
+    public IBioMiniDevice.TemplateData teacherCapturedTemplate;
+    private Bitmap teacherImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
