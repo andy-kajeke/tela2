779a388e9cba6ee6eceaa45ecfc106670c31e419
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-18 16:30:00 +0300

    added fingerprint activity, changed computer

---
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -0,0 +1,16 @@
+package com.planetsystems.tela.activities.fingerprintactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.planetsystems.tela.R;
+
+public class FingerPrintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_print);
+    }
+}
