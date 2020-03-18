933ec2d47428447af0196da47c948868ea6f752b
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-18 17:02:27 +0300

    added the reciever

---
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/DeviceBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package com.planetsystems.tela.activities.fingerprintactivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeviceBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
