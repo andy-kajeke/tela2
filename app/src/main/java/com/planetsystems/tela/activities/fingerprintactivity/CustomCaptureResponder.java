ef144594b2b39a44b6ae0e4b9c9dca6e7d13f4b7
Simon Peter Ojok <simonojok19@gmail.com>
2020-03-18 17:00:01 +0300

    added the codes

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/CustomCaptureResponder.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/CustomCaptureResponder.java
@@ -1,6 +1,31 @@
 package com.planetsystems.tela.activities.fingerprintactivity;
 
+import android.graphics.Bitmap;
+
 import com.suprema.CaptureResponder;
+import com.suprema.IBioMiniDevice;
 
 public class CustomCaptureResponder extends CaptureResponder {
+    public CustomCaptureResponder() {
+        super();
+    }
+
+    @Override
+    public void onCapture(Object contest, IBioMiniDevice.FingerState fingerState) {
+        super.onCapture(contest, fingerState);
+    }
+
+    @Override
+    public boolean onCaptureEx(
+            Object contest,
+            Bitmap bitmap,
+            IBioMiniDevice.TemplateData templateData,
+            IBioMiniDevice.FingerState fingerState) {
+        return super.onCaptureEx(contest, bitmap, templateData, fingerState);
+    }
+
+    @Override
+    public void onCaptureError(Object contest, int errorCode, String errorMessage) {
+        super.onCaptureError(contest, errorCode, errorMessage);
+    }
 }
