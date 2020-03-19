36d29091c61df79d710e37c10c0dbef1c46e5bc5
andy-kajeke <akajeke@gmail.com>
2020-03-19 11:39:02 +0300

    added

---
+++ app/src/main/java/com/planetsystems/tela/activities/Clockin_with_StaffId.java
@@ -0,0 +1,13 @@
+package com.planetsystems.tela.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clockin_with_StaffId extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clockin_with__staff_id);
+    }
+}
