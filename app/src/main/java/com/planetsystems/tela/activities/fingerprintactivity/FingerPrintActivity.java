1159e94115604074e08ceed680e479f38a0052c5
simonojok19 <simonojok19@gmail.com>
2020-03-19 12:23:36 +0300

    working on the layout now

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.planetsystems.tela.R;
 
 public class FingerPrintActivity extends AppCompatActivity {
+    public static final String ACTION_USB_PERMISSION = "com.planetsystems.tela.activities.fingerprintactivity.ACTION_USB_PERMISSION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
