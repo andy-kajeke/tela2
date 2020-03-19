d177ea2a3cca9fe777b9bf91eea5dc17d70d1391
simonojok19 <simonojok19@gmail.com>
2020-03-19 12:06:54 +0300

    copied the project

--- app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
+++ app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
@@ -7,12 +7,11 @@ import android.os.Bundle;
 
 import com.planetsystems.tela.R;
 
-public class EnrollFingerPrint extends Activity {
+public class ClockIn_with_StaffId extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enroll_finger_print);
-        setTitle(R.string.capture);
+        setContentView(R.layout.activity_clockin_with__staff_id);
     }
 }
