28bf6d084572e3d84c753b9db79af94190ec8c09
simonojok19 <simonojok19@gmail.com>
2020-03-19 17:46:39 +0300

    implemented one method

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -1,13 +1,17 @@
 package com.planetsystems.tela.activities.fingerprintactivity;
 
 import android.app.PendingIntent;
+import android.graphics.Bitmap;
 import android.hardware.usb.UsbManager;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.planetsystems.tela.R;
 import com.suprema.BioMiniFactory;
+import com.suprema.CaptureResponder;
 import com.suprema.IBioMiniDevice;
 
 public class FingerPrintActivity extends AppCompatActivity {
@@ -26,5 +30,37 @@ public class FingerPrintActivity extends AppCompatActivity {
     private TextView mLogView;
     private ScrollView mScrollLog = null;
 
+    private IBioMiniDevice.CaptureOption mCaptureOptionDefault = new IBioMiniDevice.CaptureOption();
+    private CaptureResponder mCaptureResponseDefault = new CaptureResponder() {
+        @Override
+        public boolean onCaptureEx(final Object context, final Bitmap capturedImage,
+                                   final IBioMiniDevice.TemplateData capturedTemplate,
+                                   final IBioMiniDevice.FingerState fingerState) {
+            log("onCapture : Capture successful!");
+            printState(getResources().getText(R.string.capture_single_ok));
 
+            log(((IBioMiniDevice) context).popPerformanceLog());
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (capturedImage != null) {
+                        ImageView iv = (ImageView) findViewById(R.id.imageView_fingerprint);
+                        if (iv != null) {
+                            iv.setImageBitmap(capturedImage);
+                        }
+                    }
+                }
+            });
+            return true;
+        }
+
+    };
+
+    private void printState(CharSequence message) {
+
+    }
+
+    private void log(String message) {
+
+    }
 }
