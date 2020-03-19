54fe09c528027e520e6006d622b76743dc263212
simonojok19 <simonojok19@gmail.com>
2020-03-19 16:45:19 +0300

    added on device changed

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/TelaBioMiniFactory.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/TelaBioMiniFactory.java
@@ -6,16 +6,27 @@ import android.hardware.usb.UsbManager;
 import com.suprema.BioMiniFactory;
 
 public class TelaBioMiniFactory extends BioMiniFactory {
+    private OnTelaDeviceChangeListener changeListener;
     public TelaBioMiniFactory(Context context) {
         super(context);
+        if ( changeListener != null ) {
+            changeListener = (OnTelaDeviceChangeListener) context;
+        }
     }
 
     public TelaBioMiniFactory(Context context, UsbManager usbManager) {
         super(context, usbManager);
+        if ( changeListener != null ) {
+            changeListener = (OnTelaDeviceChangeListener) context;
+        }
     }
 
     @Override
-    public void onDeviceChange(DeviceChangeEvent deviceChangeEvent, Object o) {
+    public void onDeviceChange(DeviceChangeEvent deviceChangeEvent, Object contest) {
+        changeListener.onTelaDeviceChange(deviceChangeEvent, contest);
+    }
 
+    public interface OnTelaDeviceChangeListener {
+        void onTelaDeviceChange(DeviceChangeEvent deviceChangeEvent, Object contest);
     }
 }
