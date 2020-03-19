7ac7321a57cc4e66d9d5502963195c58a0a6449c
simonojok19 <simonojok19@gmail.com>
2020-03-19 17:41:12 +0300

    started afresh

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -1,7 +1,17 @@
 package com.planetsystems.tela.activities.fingerprintactivity;
 
+import android.app.PendingIntent;
+import android.hardware.usb.UsbManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FingerPrintActivity extends AppCompatActivity {
+    //Flag.
+    public static final boolean mbUsbExternalUSBManager = false;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private UsbManager mUsbManager = null;
+    private PendingIntent mPermissionIntent= null;
+    //
+
 
 }
