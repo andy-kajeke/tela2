b3918c8b3501fa91d5f7a8fbefd3f1964c501d96
simonojok19 <simonojok19@gmail.com>
2020-03-19 17:48:56 +0300

    added the remaining method

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/FingerPrintActivity.java
@@ -3,6 +3,7 @@ package com.planetsystems.tela.activities.fingerprintactivity;
 import android.app.PendingIntent;
 import android.graphics.Bitmap;
 import android.hardware.usb.UsbManager;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ import com.planetsystems.tela.R;
 import com.suprema.BioMiniFactory;
 import com.suprema.CaptureResponder;
 import com.suprema.IBioMiniDevice;
+
+import java.util.Locale;
 
 public class FingerPrintActivity extends AppCompatActivity {
     //Flag.
@@ -61,6 +64,41 @@ public class FingerPrintActivity extends AppCompatActivity {
                 printState(getResources().getText(R.string.capture_single_fail) + "("+error+")");
         }
 
+    };
+
+    private CaptureResponder mCaptureResponsePrev = new CaptureResponder() {
+        @Override
+        public boolean onCaptureEx(final Object context, final Bitmap capturedImage,
+                                   final IBioMiniDevice.TemplateData capturedTemplate,
+                                   final IBioMiniDevice.FingerState fingerState) {
+
+            Log.d("CaptureResponsePrev", String.format(Locale.ENGLISH , "captureTemplate.size (%d) , fingerState(%s)" , capturedTemplate== null? 0 : capturedTemplate.data.length, String.valueOf(fingerState.isFingerExist)));
+            printState(getResources().getText(R.string.start_capture_ok));
+            byte[] pImage_raw =null;
+            if( (mCurrentDevice!= null && (pImage_raw = mCurrentDevice.getCaptureImageAsRAW_8() )!= null)) {
+                Log.d("CaptureResponsePrev ", String.format(Locale.ENGLISH, "pImage (%d) , FP Quality(%d)", pImage_raw.length , mCurrentDevice.getFPQuality(pImage_raw, mCurrentDevice.getImageWidth(), mCurrentDevice.getImageHeight(), 2)));
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(capturedImage != null) {
+                        ImageView iv = (ImageView) findViewById(R.id.imageView_fingerprint);
+                        if(iv != null) {
+                            iv.setImageBitmap(capturedImage);
+                        }
+                    }
+                }
+            });
+            return true;
+        }
+
+        @Override
+        public void onCaptureError(Object context, int errorCode, String error) {
+            log("onCaptureError : " + error);
+            log(((IBioMiniDevice)context).popPerformanceLog());
+            if( errorCode != IBioMiniDevice.ErrorCode.OK.value())
+                printState(getResources().getText(R.string.start_capture_fail));
+        }
     };
 
     private void printState(CharSequence message) {
