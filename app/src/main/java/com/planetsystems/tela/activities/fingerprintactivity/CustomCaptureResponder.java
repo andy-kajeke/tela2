63287d700361efd8cb62cc78f64ce89af7b07f1b
simonojok19 <simonojok19@gmail.com>
2020-03-19 16:05:49 +0300

    implemented the methods

--- app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/CustomCaptureResponder.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprintactivity/CustomCaptureResponder.java
@@ -1,18 +1,22 @@
 package com.planetsystems.tela.activities.fingerprintactivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
 import com.suprema.CaptureResponder;
 import com.suprema.IBioMiniDevice;
 
 public class CustomCaptureResponder extends CaptureResponder {
-    public CustomCaptureResponder() {
+    private OnCustomCaptureResponderResponseListener responseListener;
+    public CustomCaptureResponder(Context context) {
         super();
+        responseListener = (OnCustomCaptureResponderResponseListener) context;
     }
 
     @Override
     public void onCapture(Object contest, IBioMiniDevice.FingerState fingerState) {
         super.onCapture(contest, fingerState);
+        responseListener.onCapture(contest, fingerState);
     }
 
     @Override
@@ -21,11 +25,13 @@ public class CustomCaptureResponder extends CaptureResponder {
             Bitmap bitmap,
             IBioMiniDevice.TemplateData templateData,
             IBioMiniDevice.FingerState fingerState) {
+        responseListener.onCaptureEx(contest, bitmap, templateData, fingerState);
         return super.onCaptureEx(contest, bitmap, templateData, fingerState);
     }
 
     @Override
     public void onCaptureError(Object contest, int errorCode, String errorMessage) {
+        responseListener.onCaptureError(contest, errorCode, errorMessage);
         super.onCaptureError(contest, errorCode, errorMessage);
     }
 
