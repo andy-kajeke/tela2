b8093d19b96a4a4029aa80c658236475b97946e9
andy-kajeke <akajeke@gmail.com>
2020-03-20 15:51:41 +0300

    update

--- app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
+++ app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
@@ -1,12 +1,11 @@
-<<<<<<< HEAD:app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
+
 package com.planetsystems.tela.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.planetsystems.tela.R;
 
 public class ClockIn_with_StaffId extends Activity {
 
@@ -16,22 +15,3 @@ public class ClockIn_with_StaffId extends Activity {
         setContentView(R.layout.activity_clockin_with__staff_id);
     }
 }
-=======
-package com.planetsystems.tela.activities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.os.Bundle;
-
-import com.planetsystems.tela.R;
-
-public class ClockIn_with_StaffId extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clockin_with__staff_id);
-    }
-}
->>>>>>> 20dbe0a8887c8fe8fcca49ccbd17dc87b13a9de1:app/src/main/java/com/planetsystems/tela/activities/ClockIn_with_StaffId.java
