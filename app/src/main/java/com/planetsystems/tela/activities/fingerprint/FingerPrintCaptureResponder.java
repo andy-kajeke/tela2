ad4917bf8412e8efeb720bcf4576b0bb72634610
simonojok19 <simonojok19@gmail.com>
2020-03-20 09:45:26 +0300

    added the listener

--- app/src/main/java/com/planetsystems/tela/activities/fingerprint/FingerPrintCaptureResponder.java
+++ app/src/main/java/com/planetsystems/tela/activities/fingerprint/FingerPrintCaptureResponder.java
@@ -1,27 +1,37 @@
 package com.planetsystems.tela.activities.fingerprint;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
 import com.suprema.CaptureResponder;
 import com.suprema.IBioMiniDevice;
 
 public class FingerPrintCaptureResponder extends CaptureResponder {
-    public FingerPrintCaptureResponder() {
+    private OnFingerPrintCaptureResponseListener listener;
+    public FingerPrintCaptureResponder(Context context) {
         super();
+        listener = (OnFingerPrintCaptureResponseListener) context;
     }
 
     @Override
     public void onCapture(Object o, IBioMiniDevice.FingerState fingerState) {
         super.onCapture(o, fingerState);
+        listener.onFingerPrintCaptureResponseCapture(o, fingerState);
     }
 
     @Override
     public boolean onCaptureEx(Object o, Bitmap bitmap, IBioMiniDevice.TemplateData templateData, IBioMiniDevice.FingerState fingerState) {
-        return super.onCaptureEx(o, bitmap, templateData, fingerState);
+        return listener.onFingerPrintCaptureResponseCaptureEx(o, bitmap, templateData, fingerState);
     }
 
     @Override
     public void onCaptureError(Object o, int i, String s) {
-        super.onCaptureError(o, i, s);
+        listener.onFingerPrintCaptureResponseCaptureError(o, i, s);
+    }
+
+    public interface OnFingerPrintCaptureResponseListener {
+        void onFingerPrintCaptureResponseCapture(Object contest, IBioMiniDevice.FingerState fingerState);
+        boolean onFingerPrintCaptureResponseCaptureEx(Object o, Bitmap bitmap, IBioMiniDevice.TemplateData templateData, IBioMiniDevice.FingerState fingerState);
+        void onFingerPrintCaptureResponseCaptureError(Object contest, int errorCode, String errorMessage);
     }
 }
