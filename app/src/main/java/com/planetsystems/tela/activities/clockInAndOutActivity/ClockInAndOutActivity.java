99e728fea7d9cf8401bf4bff53b23404b27382f4
simonojok19 <simonojok19@gmail.com>
2020-03-20 15:23:12 +0300

    added the codes

--- app/src/main/java/com/planetsystems/tela/activities/clockInAndOutActivity/ClockInAndOutActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/clockInAndOutActivity/ClockInAndOutActivity.java
@@ -4,26 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.planetsystems.tela.R;
 import com.planetsystems.tela.activities.ClockIn_with_StaffId;
-import com.planetsystems.tela.activities.enrollActivity.EnrollmentActivity;
 import com.planetsystems.tela.activities.FingerPrintCaptureResponder;
+import com.planetsystems.tela.activities.enrollActivity.EnrollmentActivity;
+import com.suprema.BioMiniFactory;
 import com.suprema.IBioMiniDevice;
+import com.suprema.IUsbEventHandler;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
 
 public class ClockInAndOutActivity extends AppCompatActivity implements FingerPrintCaptureResponder.OnFingerPrintCaptureResponseListener {
 
@@ -32,6 +47,82 @@ public class ClockInAndOutActivity extends AppCompatActivity implements FingerPr
     Button btnFingerprint_In, btnStaffId_In, btnFingerprint_Out, btnStaffId_Out;
     CardView checkin, checkout, datacenter;
     Dialog checkInDialog, checkOutDialog;
+    public static final boolean mbUsbExternalUSBManager = false;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private UsbManager mUsbManager = null;
+    private PendingIntent mPermissionIntent= null;
+    //
+    private static BioMiniFactory mBioMiniFactory = null;
+    public static final int REQUEST_WRITE_PERMISSION = 786;
+    public IBioMiniDevice mCurrentDevice = null;
+    private ClockInAndOutActivity mainContext;
+
+    public final static String TAG = "BioMini Sample";
+    private TextView statusTextView;
+    private ScrollView mScrollLog = null;
+    private Intent intent;
+    private IBioMiniDevice.TemplateData capturedTemplateData;
+    private Bitmap capturedImageData;
+    private ClockInAndOutActivityViewModel printActivityViewModel;
+
+    private IBioMiniDevice.CaptureOption mCaptureOptionDefault = new IBioMiniDevice.CaptureOption();
+
+    synchronized public void printState(final CharSequence str){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                statusTextView.setText(str);
+            }
+        });
+    }
+
+    synchronized public void log(final String msg) {
+    }
+
+    synchronized public void printRev(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+//                ((TextView) findViewById(R.id.revText)).setText(msg);
+            }
+        });
+    }
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver(){
+        public void onReceive(Context context, Intent intent){
+            String action = intent.getAction();
+            if(ACTION_USB_PERMISSION.equals(action)){
+                synchronized(this){
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if(intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)){
+                        if(device != null){
+                            if( mBioMiniFactory == null) return;
+                            mBioMiniFactory.addDevice(device);
+                            log(String.format(Locale.ENGLISH ,"Initialized device count- BioMiniFactory (%d)" , mBioMiniFactory.getDeviceCount() ));
+                        }
+                    }
+                    else{
+                        Log.d(TAG, "permission denied for device"+ device);
+                    }
+                }
+            }
+        }
+    };
+
+    public void checkDevice(){
+        if(mUsbManager == null) return;
+        log("checkDevice");
+        HashMap<String , UsbDevice> deviceList = mUsbManager.getDeviceList();
+        Iterator<UsbDevice> deviceIter = deviceList.values().iterator();
+        while(deviceIter.hasNext()){
+            UsbDevice _device = deviceIter.next();
+            if( _device.getVendorId() ==0x16d1 ){
+                //Suprema vendor ID
+                mUsbManager.requestPermission(_device , mPermissionIntent);
+            }else{
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -163,6 +254,97 @@ public class ClockInAndOutActivity extends AppCompatActivity implements FingerPr
 
         checkOutDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         checkOutDialog.show();
+    }
+
+    void handleDevChange(IUsbEventHandler.DeviceChangeEvent event, Object dev) {
+        if (event == IUsbEventHandler.DeviceChangeEvent.DEVICE_ATTACHED && mCurrentDevice == null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int cnt = 0;
+                    while (mBioMiniFactory == null && cnt < 20) {
+                        SystemClock.sleep(1000);
+                        cnt++;
+                    }
+                    if (mBioMiniFactory != null) {
+                        mCurrentDevice = mBioMiniFactory.getDevice(0);
+                        printState(getResources().getText(R.string.device_attached));
+                        Log.d(TAG, "mCurrentDevice attached : " + mCurrentDevice);
+                        if (mCurrentDevice != null /*&& mCurrentDevice.getDeviceInfo() != null*/) {
+                            log(" DeviceName : " + mCurrentDevice.getDeviceInfo().deviceName);
+                            log("         SN : " + mCurrentDevice.getDeviceInfo().deviceSN);
+                            log("SDK version : " + mCurrentDevice.getDeviceInfo().versionSDK);
+                        }
+                    }
+                }
+            }).start();
+        } else if (mCurrentDevice != null && event == IUsbEventHandler.DeviceChangeEvent.DEVICE_DETACHED && mCurrentDevice.isEqual(dev)) {
+            printState(getResources().getText(R.string.device_detached));
+            Log.d(TAG, "mCurrentDevice removed : " + mCurrentDevice);
+            mCurrentDevice = null;
+        }
+    }
+
+    void restartBioMini() {
+        if(mBioMiniFactory != null) {
+            mBioMiniFactory.close();
+        }
+        if( mbUsbExternalUSBManager ){
+            mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
+            mBioMiniFactory = new BioMiniFactory(mainContext, mUsbManager){
+                @Override
+                public void onDeviceChange(DeviceChangeEvent event, Object dev) {
+                    log("----------------------------------------");
+                    log("onDeviceChange : " + event + " using external usb-manager");
+                    log("----------------------------------------");
+                    handleDevChange(event, dev);
+                }
+            };
+            //
+            mPermissionIntent = PendingIntent.getBroadcast(this,0,new Intent(ACTION_USB_PERMISSION),0);
+            IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+            registerReceiver(mUsbReceiver, filter);
+            checkDevice();
+        }else {
+            mBioMiniFactory = new BioMiniFactory(mainContext) {
+                @Override
+                public void onDeviceChange(DeviceChangeEvent event, Object dev) {
+                    log("----------------------------------------");
+                    log("onDeviceChange : " + event);
+                    log("----------------------------------------");
+                    handleDevChange(event, dev);
+                }
+            };
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBioMiniFactory != null) {
+            mBioMiniFactory.close();
+            mBioMiniFactory = null;
+        }
+        if( mbUsbExternalUSBManager ){
+            unregisterReceiver(mUsbReceiver);
+        }
+        super.onDestroy();
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},  REQUEST_WRITE_PERMISSION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            log("permission granted");
+        }
+    }
+    @Override
+    public void onPostCreate(Bundle savedInstanceState){
+        requestPermission();
+        super.onPostCreate(savedInstanceState);
     }
 
     @Override
