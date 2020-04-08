19066c55225a21dba0cec35bd7a11125a4732629
simonojok19 <simonojok19@gmail.com>
2020-04-08 12:48:23 +0300

    created some packages

---
+++ app/src/main/java/com/planetsystems/tela/services/syncteachers/SyncTeacherDownloadService.java
@@ -0,0 +1,18 @@
+package com.planetsystems.tela.services.syncteachers;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+public class SyncTeacherDownloadService extends IntentService {
+
+    public SyncTeacherDownloadService() {
+        super("SyncTeacherDownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+}
---
+++ app/src/main/java/com/planetsystems/tela/services/syncteachers/SyncTeachers.java
@@ -0,0 +1,4 @@
+package com.planetsystems.tela.services.syncteachers;
+
+public class SyncTeachers {
+}
