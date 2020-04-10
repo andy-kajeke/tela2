1488e87114cfec33d250142a8a808edafc661114
simonojok19 <simonojok19@gmail.com>
2020-04-10 17:52:39 +0300

    added two view models

---
+++ app/src/main/java/com/planetsystems/tela/staff/administration/AdminSideActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.planetsystems.tela.staff.administration;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class AdminSideActivityViewModel extends AndroidViewModel {
+    public AdminSideActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
---
+++ app/src/main/java/com/planetsystems/tela/staff/regularStaff/TeacherHomeActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.planetsystems.tela.staff.regularStaff;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class TeacherHomeActivityViewModel extends AndroidViewModel {
+    public TeacherHomeActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
