e84d4bc18dbc68985c533ac968ea84fc9a02db76
simonojok19 <simonojok19@gmail.com>
2020-04-10 19:41:56 +0300

    added some new codes

--- app/src/main/java/com/planetsystems/tela/utils/BitmapConverter.java
+++ app/src/main/java/com/planetsystems/tela/utils/BitmapConverter.java
@@ -8,13 +8,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 public class BitmapConverter {
-    public static String encodeToBase64(Bitmap bitmap) {
+    public static String encodeBitmapToBase64(Bitmap bitmap) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
         return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
     }
 
-    public static Bitmap decodeBase64(String base64BitmapImage) {
+    public static Bitmap decodeBase64ToBitmap(String base64BitmapImage) {
         byte[] decodedByte = Base64.decode(base64BitmapImage, 0);
         return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
     }
