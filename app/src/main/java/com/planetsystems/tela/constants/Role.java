62ae5e113df2c9199457ee4db8a0f2ecb9f5723b
simonojok19 <simonojok19@gmail.com>
2020-04-10 16:56:47 +0300

    refactored

--- app/src/main/java/com/planetsystems/tela/constants/Role.java
+++ app/src/main/java/com/planetsystems/tela/constants/Role.java
@@ -1,6 +1,6 @@
 package com.planetsystems.tela.constants;
 
-public class Utils {
+public class Role {
     public static final String TEACHER_ROLE = "Teacher";
     public static final String HEAD_TEACHER_ROLE = "Head Teacher";
 }
