733ad8d7d50eb845ebb5409365f63a1e586d5b1e
simonojok19 <simonojok19@gmail.com>
2020-04-10 12:24:24 +0300

    added the second view model

---
+++ app/src/main/java/com/planetsystems/tela/activities/clockwithstaffid/ClockInWithStaffIdActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.planetsystems.tela.activities.clockwithstaffid;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ClockInWithStaffIdActivityViewModel extends AndroidViewModel {
+    public ClockInWithStaffIdActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
