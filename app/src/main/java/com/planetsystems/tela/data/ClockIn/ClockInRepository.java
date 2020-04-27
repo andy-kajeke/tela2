b7d6d8fc21ae18546133df5d03d5eb49f1ac6d54
simonojok19 <simonojok19@gmail.com>
2020-04-27 12:44:17 +0300

    created syn clock in repository

--- app/src/main/java/com/planetsystems/tela/data/ClockIn/ClockInRepository.java
+++ app/src/main/java/com/planetsystems/tela/data/ClockIn/ClockInRepository.java
@@ -1,4 +1,12 @@
 package com.planetsystems.tela.data.ClockIn;
 
+import android.app.Application;
+
+import com.planetsystems.tela.data.TelaRoomDatabase;
+
 public class ClockInRepository {
+    private SyncClockInDao syncClockInDao;
+    public ClockInRepository(TelaRoomDatabase telaRoomDatabase) {
+        syncClockInDao = telaRoomDatabase.getSyncClockInDao();
+    }
 }
