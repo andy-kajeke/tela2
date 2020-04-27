f4921b9a773eef21a9bede780bf1e5c4899f759b
simonojok19 <simonojok19@gmail.com>
2020-04-27 10:31:01 +0300

    renamed some variable

--- app/src/main/java/com/planetsystems/tela/constants/Urls.java
+++ app/src/main/java/com/planetsystems/tela/constants/Urls.java
@@ -8,5 +8,5 @@ public class Urls {
     public static final String SYNC_TEACHER_URL = BASE_URL + "teachers/" + DEVICE_IMEI;
     public static final String CLOCK_IN = BASE_URL + "clockin";
     public static final String CLOCK_OUT = BASE_URL + "clockout";
-    public static final String TIMETABLE = BASE_URL + "timetable/" + DEVICE_IMEI;
+    public static final String SYNC_TIME_TABLE_URL = BASE_URL + "timetable/" + DEVICE_IMEI;
 }
