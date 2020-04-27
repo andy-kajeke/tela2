be5dcdd21dc09d2b64d4bc43eb1fa566cf6afd53
simonojok19 <simonojok19@gmail.com>
2020-04-27 12:47:33 +0300

    created singliton

--- app/src/main/java/com/planetsystems/tela/data/ClockIn/ClockInRepository.java
+++ app/src/main/java/com/planetsystems/tela/data/ClockIn/ClockInRepository.java
@@ -5,8 +5,21 @@ import android.app.Application;
 import com.planetsystems.tela.data.TelaRoomDatabase;
 
 public class ClockInRepository {
+    private static ClockInRepository INSTANCE;
     private SyncClockInDao syncClockInDao;
-    public ClockInRepository(TelaRoomDatabase telaRoomDatabase) {
+
+    private ClockInRepository(TelaRoomDatabase telaRoomDatabase) {
         syncClockInDao = telaRoomDatabase.getSyncClockInDao();
+    }
+
+    public static ClockInRepository getInstance(final TelaRoomDatabase telaRoomDatabase) {
+        if (INSTANCE == null) {
+            synchronized (ClockInRepository.class) {
+                if ( INSTANCE == null ) {
+                    INSTANCE = new ClockInRepository(telaRoomDatabase);
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
