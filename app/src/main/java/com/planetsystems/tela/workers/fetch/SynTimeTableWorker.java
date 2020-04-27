f2d90317f1dd3241563535db67e8f55167629964
simonojok19 <simonojok19@gmail.com>
2020-04-27 10:29:53 +0300

    working on the room worker

--- app/src/main/java/com/planetsystems/tela/workers/fetch/SynTimeTableWorker.java
+++ app/src/main/java/com/planetsystems/tela/workers/fetch/SynTimeTableWorker.java
@@ -6,9 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.planetsystems.tela.data.TelaRoomDatabase;
+import com.planetsystems.tela.data.timetable.SyncTimeTableDao;
+
 public class SynTimeTableWorker extends Worker {
+    private SyncTimeTableDao syncTimeTableDao;
+
     public SynTimeTableWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        TelaRoomDatabase telaRoomDatabase = TelaRoomDatabase.getInstance(context);
+        syncTimeTableDao = telaRoomDatabase.getSyncTimeTableDao();
     }
 
     @NonNull
