402f863b4f4eaf529ce6d89db6cc6ef8753a1a35
simonojok19 <simonojok19@gmail.com>
2020-04-27 13:33:21 +0300

    added all

--- app/src/main/java/com/planetsystems/tela/data/ClockIn/SyncClockIn.java
+++ app/src/main/java/com/planetsystems/tela/data/ClockIn/SyncClockIn.java
@@ -37,4 +37,102 @@ public class SyncClockIn {
 
     @ColumnInfo(name = SyncClockInTableConstants.COLUMN_SCHOOL_ID)
     private String schoolId;
+
+    public SyncClockIn(String employeeNo, String employeeId, String latitude, String longitude, String clockInDate, String day, String clockInTime, String schoolId) {
+        this.employeeNo = employeeNo;
+        this.employeeId = employeeId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.clockInDate = clockInDate;
+        this.day = day;
+        this.clockInTime = clockInTime;
+        this.schoolId = schoolId;
+    }
+
+    public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(int primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public String getEmployeeNo() {
+        return employeeNo;
+    }
+
+    public void setEmployeeNo(String employeeNo) {
+        this.employeeNo = employeeNo;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getClockInDate() {
+        return clockInDate;
+    }
+
+    public void setClockInDate(String clockInDate) {
+        this.clockInDate = clockInDate;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getClockInTime() {
+        return clockInTime;
+    }
+
+    public void setClockInTime(String clockInTime) {
+        this.clockInTime = clockInTime;
+    }
+
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    @Override
+    public String toString() {
+        return "SyncClockIn{" +
+                "primaryKey=" + primaryKey +
+                ", employeeNo='" + employeeNo + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", latitude='" + latitude + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", clockInDate='" + clockInDate + '\'' +
+                ", day='" + day + '\'' +
+                ", clockInTime='" + clockInTime + '\'' +
+                ", schoolId='" + schoolId + '\'' +
+                '}';
+    }
 }
