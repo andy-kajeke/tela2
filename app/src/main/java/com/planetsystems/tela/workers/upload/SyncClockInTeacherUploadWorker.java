dff2ddfec693d117d3c286abb7388247cba26857
simonojok19 <simonojok19@gmail.com>
2020-05-06 09:48:05 +0300

    working on the worker

--- app/src/main/java/com/planetsystems/tela/workers/upload/SyncClockInTeacherUploadWorker.java
+++ app/src/main/java/com/planetsystems/tela/workers/upload/SyncClockInTeacherUploadWorker.java
@@ -6,9 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.planetsystems.tela.data.ClockIn.SyncClockInDao;
+import com.planetsystems.tela.data.TelaRoomDatabase;
+
 public class SyncClockInTeacherUploadWorker extends Worker {
+    SyncClockInDao syncClockInDao;
+
+
     public SyncClockInTeacherUploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        syncClockInDao = TelaRoomDatabase.getInstance(getApplicationContext()).getSyncClockInDao();
     }
 
     @NonNull
