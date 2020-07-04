13956dcdfc20cad67225e3e830553cf989ba863a
simonojok19 <simonojok19@gmail.com>
2020-07-04 20:27:13 +0300

    added everything

--- app/src/main/java/com/planetsystems/tela/activities/clockInAndOutActivity/ClockInAndOutActivityViewModel.java
+++ app/src/main/java/com/planetsystems/tela/activities/clockInAndOutActivity/ClockInAndOutActivityViewModel.java
@@ -57,6 +57,8 @@ public class ClockInAndOutActivityViewModel extends AndroidViewModel {
                         )
                 );
                 return  teacher;
+            } else {
+                return null;
             }
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
