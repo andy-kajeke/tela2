dfaf6489500c5a94f858ac3e09d3e44ab00a6367
simonojok19 <simonojok19@gmail.com>
2020-07-05 18:23:11 +0300

    added execution log repository

--- app/src/main/java/com/planetsystems/tela/logs/ExecutionLogDao.java
+++ app/src/main/java/com/planetsystems/tela/logs/ExecutionLogDao.java
@@ -1,4 +1,17 @@
 package com.planetsystems.tela.logs;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface ExecutionLogDao {
+    @Insert
+    void logMessage(ExecutionLog executionLog);
+
+    @Query("SELECT * FROM " + ExecutionLogConstants.TABLE_NAME)
+    LiveData<List<ExecutionLog>> getLogs();
 }
---
+++ app/src/main/java/com/planetsystems/tela/logs/ExecutionLogRepository.java
@@ -0,0 +1,4 @@
+package com.planetsystems.tela.logs;
+
+public class ExecutionLogRepository {
+}
