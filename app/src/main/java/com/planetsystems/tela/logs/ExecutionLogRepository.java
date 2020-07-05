d2f37bcd09f6f560cd76c872c276b95590746d3e
simonojok19 <simonojok19@gmail.com>
2020-07-05 18:31:37 +0300

    added the add

--- app/src/main/java/com/planetsystems/tela/logs/ExecutionLogRepository.java
+++ app/src/main/java/com/planetsystems/tela/logs/ExecutionLogRepository.java
@@ -1,4 +1,40 @@
 package com.planetsystems.tela.logs;
 
+import androidx.lifecycle.LiveData;
+
+import com.planetsystems.tela.data.ClockIn.ClockInRepository;
+import com.planetsystems.tela.data.TelaRoomDatabase;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
 public class ExecutionLogRepository {
+    private static ExecutionLogRepository INSTANCE;
+    private ExecutionLogDao executionLogDao;
+
+    private ExecutionLogRepository (TelaRoomDatabase telaRoomDatabase) {
+        executionLogDao = telaRoomDatabase.getExecutionLogDao();
+    }
+
+    public static ExecutionLogRepository getInstance(TelaRoomDatabase telaRoomDatabase) {
+        if (INSTANCE == null) {
+            synchronized (ClockInRepository.class) {
+                if ( INSTANCE == null ) {
+                    INSTANCE = new ExecutionLogRepository(telaRoomDatabase);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public LiveData<List<ExecutionLog>> getLogs() throws ExecutionException, InterruptedException {
+        Callable<LiveData<List<ExecutionLog>>> callable = new Callable<LiveData<List<ExecutionLog>>>() {
+            @Override
+            public LiveData<List<ExecutionLog>> call() throws Exception {
+                return executionLogDao.getLogs();
+            }
+        };
+        return TelaRoomDatabase.db_executor.submit(callable).get();
+    }
 }
