fa138cc7181bc1372a7888f0619fe556dcbb4a6c
simonojok19 <simonojok19@gmail.com>
2020-07-05 23:38:03 +0300

    updated getters and setters

--- app/src/main/java/com/planetsystems/tela/data/logs/ExecutionLog.java
+++ app/src/main/java/com/planetsystems/tela/data/logs/ExecutionLog.java
@@ -37,5 +37,95 @@ public class ExecutionLog {
     @ColumnInfo(name = ExecutionLogConstants.METHOD_NAME)
     private String methodName;
 
+    public ExecutionLog(String message, String date, String time, String className, String deviceNo, String school, String data, String lineNumber, String methodName) {
+        this.message = message;
+        this.date = date;
+        this.time = time;
+        this.className = className;
+        this.deviceNo = deviceNo;
+        this.school = school;
+        this.data = data;
+        this.lineNumber = lineNumber;
+        this.methodName = methodName;
+    }
 
+    public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(int primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getDeviceNo() {
+        return deviceNo;
+    }
+
+    public void setDeviceNo(String deviceNo) {
+        this.deviceNo = deviceNo;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(String lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
 }
