d404a8c64a89f4eb11e1d2ce8da32169455b48c0
simonojok19 <simonojok19@gmail.com>
2020-07-05 19:15:41 +0300

    added the codes

--- app/src/main/java/com/planetsystems/tela/activities/logs/LogActivity.java
+++ app/src/main/java/com/planetsystems/tela/activities/logs/LogActivity.java
@@ -1,16 +1,23 @@
 package com.planetsystems.tela.activities.logs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.planetsystems.tela.R;
 
 public class LogActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LogActivityViewModel logActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        recyclerView = findViewById(R.id.execution_log_recyclerview);
+        logActivityViewModel = new ViewModelProvider(this).get(LogActivityViewModel.class);
     }
 }
