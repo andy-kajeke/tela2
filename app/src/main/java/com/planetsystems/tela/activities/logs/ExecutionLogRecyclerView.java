6790b7694f51513647455d979fccb45cb7522bc0
simonojok19 <simonojok19@gmail.com>
2020-07-05 19:18:04 +0300

    adding view holder

--- app/src/main/java/com/planetsystems/tela/activities/logs/ExecutionLogRecyclerView.java
+++ app/src/main/java/com/planetsystems/tela/activities/logs/ExecutionLogRecyclerView.java
@@ -1,4 +1,21 @@
 package com.planetsystems.tela.activities.logs;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.planetsystems.tela.logs.ExecutionLog;
+
 public class ExecutionLogRecyclerView {
+    public static class ExecutionLogViewHold extends RecyclerView.ViewHolder {
+
+        public ExecutionLogViewHold(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bindViewHolder(ExecutionLog executionLog) {
+
+        }
+    }
 }
