f8641d491dfbf0cb79d5d9c1884aa8bc79bfc897
simonojok19 <simonojok19@gmail.com>
2020-07-05 18:40:33 +0300

    working on execution log

--- app/src/main/java/com/planetsystems/tela/activities/logs/LogActivityViewModel.java
+++ app/src/main/java/com/planetsystems/tela/activities/logs/LogActivityViewModel.java
@@ -1,4 +1,31 @@
 package com.planetsystems.tela.activities.logs;
 
-public class LogActivityViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.planetsystems.tela.MainRepository;
+import com.planetsystems.tela.logs.ExecutionLog;
+import com.planetsystems.tela.logs.ExecutionLogRepository;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class LogActivityViewModel extends AndroidViewModel {
+    private ExecutionLogRepository executionLogRepository;
+    public LogActivityViewModel(@NonNull Application application) {
+        super(application);
+        executionLogRepository = MainRepository.getInstance(application).getExecutionLogRepository();
+    }
+
+    public LiveData<List<ExecutionLog>> getLogs() {
+        try {
+            return executionLogRepository.getLogs();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
