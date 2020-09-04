ffec5eacc8adcda7725688d816cd7919ded237cc
simonojok19 <simonojok19@gmail.com>
2020-09-04 07:19:46 +0300

    adding the dialog

---
+++ app/src/main/java/com/planetsystems/tela/activities/dialogs/PhoneNumberDialogActivity.java
@@ -0,0 +1,18 @@
+package com.planetsystems.tela.activities.dialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PhoneNumberDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number_dialog);
+    }
+
+    public void saveResult(View view) {
+    }
+}
